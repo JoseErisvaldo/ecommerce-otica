05df09a8a40eb423bd011532cc7c084fa12dd1df
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-11-17 19:57:25 -0300

    Fix: name layout > Container

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,6 +1,6 @@
 import { Card } from "@material-tailwind/react";
 
-export default function LayoutAdmin ({children}) {
+export default function Container ({children}) {
   return(
     <Card className="p-5 mt-5">
       {children}
